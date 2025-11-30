5a73cd6932eb4967e57e909e18abdd3eaf2d8c07
yoshonabee <jhann0120@gmail.com>
2025-11-30 21:25:33 +0800

    chores: remove cors header and add prefix to api

--- proxy/main.py
+++ proxy/main.py
@@ -43,15 +43,7 @@ app = FastAPI(
     description="Proxy server for Raspberry Pi video streaming",
     version="1.0.0",
     lifespan=lifespan,
-)
-
-# CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    prefix="/api",
 )
 
 
